f9d3e826979dc2717c934b60a7440db627bbf689
Jordan Schalm <jordan.schalm@gmail.com>
2016-09-21 19:05:22 -0700

    upversion and remove some silly airbnb rules

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = {
 		"block-spacing": ["error", "never"],
 		"class-methods-use-this": 0,
 		"comma-dangle": ["error", "never"],
+		"import/imports-first": 0,
 		"import/no-extraneous-dependencies": 0,
 		"indent": ["error", "tab", {SwitchCase: 1}],
 		"new-cap": 0,
@@ -20,8 +21,10 @@ module.exports = {
 		"no-tabs": 0,
 		"object-curly-spacing": 0,
 		"react/jsx-closing-bracket-location": ["error", "after-props"],
+		"react/jsx-filename-extension": 0,
 		"react/jsx-indent": ["error", "tab"],
 		"react/jsx-indent-props": ["error", "tab"],
+		"react/prefer-stateless-function": ["warning"],
 		"react/sort-comp": 0,
 		"sort-imports": ["error", {ignoreCase: true}],
 		"space-before-function-paren": 0
